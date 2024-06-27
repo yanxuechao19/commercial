97414e5228039e944b78563a256c30b2d845abf5
zhaoyang <zhaoyang@frogcool.com>
2024-06-27 20:00:49 +0800

    commercial

---
+++ src/main/java/com/commercial/Richfunction/SplitStreamByFilter.java
@@ -0,0 +1,10 @@
+package com.commercial.Richfunction;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+// todo 将电商网站收集到的用户行为数据进行一个拆分，根据类型（type）的不同，分为“Songsong”的浏览数据、“Bingbing”的浏览数据等。
+public class SplitStreamByFilter {
+    public static void main(String[] args) {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+    }
+}
