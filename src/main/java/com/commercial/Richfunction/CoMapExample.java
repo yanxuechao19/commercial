1189e8e93e4723c97194b7e36aaca6e71b5168ff
yanxuechao19 <yanxuechao19@gmail.com>
2024-06-28 23:59:58 +0800

    0629

--- src/main/java/com/commercial/Richfunction/CoMapExample.java
+++ src/main/java/com/commercial/Richfunction/CoMapExample.java
@@ -2,27 +2,31 @@ package com.commercial.Richfunction;
 
 import org.apache.flink.streaming.api.datastream.ConnectedStreams;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.co.CoMapFunction;
 
 //TODO 流的联合虽然简单，不过受限于数据类型不能改变，灵活性大打折扣，所以实际应用较少出现。除了联合（union），Flink还提供了另外一种方便的合流操作——连接（connect）。 连接无需考虑数据类型，联合需要考虑
 public class CoMapExample {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         DataStreamSource<Integer> stream1 = env.fromElements(1, 2, 3);
         DataStreamSource<Long> stream2 = env.fromElements(1L, 2L, 3L);
         ConnectedStreams<Integer, Long> connectedStreams = stream1.connect(stream2);
-/*        connectedStreams.map(new CoMapFunction<Integer, Long, String>() {
+
+        SingleOutputStreamOperator<String> map = connectedStreams.map(new CoMapFunction<Integer, Long, String>() {
             @Override
             public String map1(Integer value) throws Exception {
-                return  ""+ value;
+                return "Integer:" + value;
             }
 
             @Override
             public String map2(Long value) throws Exception {
-                return "";
+                return "Long:" + value;
             }
-        })*/
+        });
+            map.print();
+            env.execute();
 
     }
 }
