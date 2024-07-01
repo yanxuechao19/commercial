97b74cbe210ff0c955558b4769e294f1c9c8d95e
zhaoyang <zhaoyang@frogcool.com>
2024-07-01 13:47:58 +0800

    commercial

--- src/main/java/com/commercial/Richfunction/CoMapExample.java
+++ src/main/java/com/commercial/Richfunction/CoMapExample.java
@@ -1,5 +1,6 @@
 package com.commercial.Richfunction;
 
+import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.streaming.api.datastream.ConnectedStreams;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
@@ -13,6 +14,7 @@ public class CoMapExample {
         DataStreamSource<Integer> stream1 = env.fromElements(1, 2, 3);
         DataStreamSource<Long> stream2 = env.fromElements(1L, 2L, 3L);
         ConnectedStreams<Integer, Long> connectedStreams = stream1.connect(stream2);
+//todo 表示分别对两条流中的数据执行map操作。这个接口有三个类型参数，依次表示第一条流、第二条流，以及合并后的流中的数据类型。需要实现的方法也非常直白：.map1()就是对第一条流中数据的map操作，.map2()则是针对第二条流。
 
         SingleOutputStreamOperator<String> map = connectedStreams.map(new CoMapFunction<Integer, Long, String>() {
             @Override
@@ -25,8 +27,35 @@ public class CoMapExample {
                 return "Long:" + value;
             }
         });
-            map.print();
-            env.execute();
+            map.print("map1连接流");
+
+            //todo 值得一提的是，ConnectedStreams也可以直接调用.keyBy()进行按键分区的操作，得到的还是一个ConnectedStreams：
+        //todo 这里传入两个参数keySelector1和keySelector2，是两条流中各自的键选择器；当然也可以直接传入键的位置值（keyPosition），或者键的字段名（field），这与普通的keyBy用法完全一致。ConnectedStreams进行keyBy操作，其实就是把两条流中key相同的数据放到了一起，然后针对来源的流再做各自处理，这在一些场景下非常有用。
+
+        ConnectedStreams<Integer, Long> integerLongConnectedStreams = connectedStreams.keyBy(new keySelector1(), new keySelector2());
+        SingleOutputStreamOperator<String> map2 = integerLongConnectedStreams.map(new CoMapFunction<Integer, Long, String>() {
+            @Override
+            public String map1(Integer value) throws Exception {
+                return "integer :" + value;
+            }
+
+            @Override
+            public String map2(Long value) throws Exception {
+                return "long " + value;
+            }
+        });
+        map2.print("map2连接流");
+        env.execute();
 
     }
+    public  static  class  keySelector1 implements KeySelector<Integer,String>{
+        @Override
+        public String getKey(Integer value) throws Exception {
+            return value.toString();
+        }}
+    public  static  class  keySelector2 implements KeySelector<Long,String>{
+        @Override
+        public String getKey(Long value) throws Exception {
+            return value.toString();
+        }}
 }
